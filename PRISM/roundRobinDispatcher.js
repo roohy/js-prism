3fc83794f6e3dcc67b539beb8a2c7aa90fab4eb7
roohy <shemirani.r@gmail.com>
2014-12-07 15:44:33 -0800

    first complete core version of javascript prism

---
+++ PRISM/roundRobinDispatcher.js
@@ -0,0 +1,29 @@
+
+var prism = prism || {};
+prism.core = prism.core || {};
+
+prism.core.roundRobinDispatcher = function(scheduler , n){
+    this.DEFAULT_COUNT = 10;
+    this.threadCount = 0;
+    this.workerThreads = [];
+    this.scheduler = typeof scheduler !== "undefined" ? scheduler : null;
+    this.n = typeof n !== "undefined" ? n : this.DEFAULT_COUNT;
+    for( var i =  0 ; i < this.n ; i++){
+        var temp = new prism.core.workerThread(""+i , this , this.scheduler);
+    }
+
+
+};
+
+prism.core.roundRobinDispatcher.prototype.start = function(){
+    for ( var i = 0 ; i < this.n ; i++){
+        this.workerThreads[i].run();
+    }
+
+};
+
+prism.core.roundRobinDispatcher.prototype.stop = function(){
+    for ( var i = 0 ; i < this.n ; i++){
+        this.workerThreads[i].keepWorking = false;
+    }
+};
